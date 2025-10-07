c9b51a37770ffcd5e37ac5d8846a3b012255b3c1
sntooxmw <mariaeduardajoviniano@gmail.com>
2025-10-07 16:09:04 -0300

    Add files via upload

---
+++ Atividade 9/Atividade 9/Program.cs
@@ -0,0 +1,16 @@
+﻿double Apples;
+double Pagar;
+
+Console.WriteLine("Você irá comprar quantas maçãs?");
+Apples = double.Parse(Console.ReadLine());
+
+if (Apples < 25)
+{
+    Pagar = Apples * 2.73;
+    Console.WriteLine($"Você irá pagar R${Pagar:F2}");
+}
+else
+{
+    Pagar = Apples * 2.00;
+    Console.WriteLine($"Você irá pagar R${Pagar:F2}");
+}
