2bcf8aa7f3aedbf7846ed2f8a3646d325a0a0db0
sntooxmw <mariaeduardajoviniano@gmail.com>
2025-10-07 16:52:50 -0300

    Add files via upload

---
+++ Atividade 12/Atividade 12/Program.cs
@@ -0,0 +1,16 @@
+﻿int Valor;
+int Valor2;
+
+Console.WriteLine("Digite o primeiro número");
+Valor = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite o segundo número");
+Valor2 = int.Parse(Console.ReadLine());
+
+if (Valor > Valor2)
+{
+    Console.WriteLine($"{Valor} é maior que {Valor2}");
+}
+else
+{
+    Console.WriteLine($"{Valor2} é maior que {Valor}");
+}
