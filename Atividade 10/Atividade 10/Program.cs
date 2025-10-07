b20613a2191f66bdab6d3d8b87fa13bccb56e47c
sntooxmw <mariaeduardajoviniano@gmail.com>
2025-10-07 16:26:19 -0300

    Add files via upload

---
+++ Atividade 10/Atividade 10/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.Design;
+
+double Nota1 = 0;
+double Nota2 = 0;
+
+Console.WriteLine("Digite a sua primeira nota");
+Nota1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Digite a sua segunda nota");
+Nota2 = double.Parse(Console.ReadLine());
+
+if (Nota1 / Nota2 > 6)
+{
+    Console.WriteLine("Você foi Aprovado!");
+}
+else
+{
+    Console.WriteLine("Você foi Reprovado!");
+}
