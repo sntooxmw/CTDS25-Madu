7c9cbbfff340b50606a1a678037b5423e3e0171f
sntooxmw <mariaeduardajoviniano@gmail.com>
2025-10-07 15:51:24 -0300

    Add files via upload

---
+++ Atividade 8/Atividade 8/Program.cs
@@ -0,0 +1,13 @@
+﻿int Valor;
+
+Console.WriteLine("Digite um número");
+Valor = int.Parse(Console.ReadLine());
+
+if (Valor > 0)
+{
+    Console.WriteLine("Seu número é positivo!");
+}
+else
+{
+    Console.WriteLine("Seu número é negativo!");
+}
