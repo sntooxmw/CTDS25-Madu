da3fab028c9f5116dad2895b7263cd95a07dd43a
sntooxmw <mariaeduardajoviniano@gmail.com>
2025-10-07 15:04:01 -0300

    Add files via upload

---
+++ Atividade 4/Atividade 4/Program.cs
@@ -0,0 +1,7 @@
+﻿int Idade;
+int Data;
+
+Console.WriteLine("Qual ano em que você nasceu?");
+Idade = int.Parse(Console.ReadLine());
+Data = (2025 - Idade) * 365;
+Console.WriteLine($"Você tem {Data} dias de vida.");
