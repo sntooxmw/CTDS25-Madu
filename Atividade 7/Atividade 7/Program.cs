7922f1c481e30a3f462b42732a8235860e423174
sntooxmw <mariaeduardajoviniano@gmail.com>
2025-10-07 15:14:54 -0300

    Add files via upload

---
+++ Atividade 7/Atividade 7/Program.cs
@@ -0,0 +1,13 @@
+﻿int Num;
+
+Console.WriteLine("Digite um Número");
+Num = int.Parse(Console.ReadLine());
+
+if (Num > 57)
+{
+    Console.WriteLine("Seu Número é maior que 57");
+}
+else
+{
+    Console.WriteLine("Seu Número não é maior que 57");
+}
