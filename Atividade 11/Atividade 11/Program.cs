213f5192ceaea9c0b7ac6a407717a5f02f6307e9
sntooxmw <mariaeduardajoviniano@gmail.com>
2025-10-07 16:39:24 -0300

    Add files via upload

---
+++ Atividade 11/Atividade 11/Program.cs
@@ -0,0 +1,16 @@
+﻿int Ano;
+int Data;
+
+Console.WriteLine("Em que ano você nasceu?");
+Ano = int.Parse(Console.ReadLine());
+
+Data = 2025 - Ano;
+
+if (Data >= 21)
+{
+    Console.WriteLine("Você já tem a idade adequada para votar!");
+}
+else
+{
+    Console.WriteLine("Você não tem idade adequada para votar!");
+}
