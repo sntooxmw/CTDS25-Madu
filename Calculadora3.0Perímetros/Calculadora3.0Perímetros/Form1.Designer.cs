d01b47832202c960b54ff01d23d36ff3c272d842
sntooxmw <mariaeduardajoviniano@gmail.com>
2025-11-18 17:28:33 -0300

    Add files via upload

---
+++ Calculadora3.0Perímetros/Calculadora3.0Perímetros/Form1.Designer.cs
@@ -0,0 +1,342 @@
+﻿namespace Calculadora3._0Perímetros
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            bindingSource1 = new BindingSource(components);
+            bnt7 = new Button();
+            bnt8 = new Button();
+            bnt9 = new Button();
+            bnt4 = new Button();
+            bnt5 = new Button();
+            bnt6 = new Button();
+            bnt1 = new Button();
+            bnt2 = new Button();
+            bntVírgula = new Button();
+            bnt0 = new Button();
+            bntDeletar = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            fileSystemWatcher1 = new FileSystemWatcher();
+            label4 = new Label();
+            txtBase = new TextBox();
+            txtLargura = new TextBox();
+            txtAltura = new TextBox();
+            bntResultado = new Button();
+            bnt3 = new Button();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)bindingSource1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)fileSystemWatcher1).BeginInit();
+            SuspendLayout();
+            // 
+            // bnt7
+            // 
+            bnt7.BackColor = Color.GhostWhite;
+            bnt7.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            bnt7.Location = new Point(158, 194);
+            bnt7.Name = "bnt7";
+            bnt7.Size = new Size(105, 92);
+            bnt7.TabIndex = 0;
+            bnt7.Text = "7";
+            bnt7.UseVisualStyleBackColor = false;
+            // 
+            // bnt8
+            // 
+            bnt8.BackColor = Color.GhostWhite;
+            bnt8.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            bnt8.Location = new Point(269, 194);
+            bnt8.Name = "bnt8";
+            bnt8.Size = new Size(105, 92);
+            bnt8.TabIndex = 1;
+            bnt8.Text = "8";
+            bnt8.UseVisualStyleBackColor = false;
+            // 
+            // bnt9
+            // 
+            bnt9.BackColor = Color.GhostWhite;
+            bnt9.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            bnt9.Location = new Point(380, 194);
+            bnt9.Name = "bnt9";
+            bnt9.Size = new Size(105, 92);
+            bnt9.TabIndex = 2;
+            bnt9.Text = "9";
+            bnt9.UseVisualStyleBackColor = false;
+            // 
+            // bnt4
+            // 
+            bnt4.BackColor = Color.GhostWhite;
+            bnt4.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            bnt4.Location = new Point(158, 292);
+            bnt4.Name = "bnt4";
+            bnt4.Size = new Size(105, 92);
+            bnt4.TabIndex = 3;
+            bnt4.Text = "4";
+            bnt4.UseVisualStyleBackColor = false;
+            // 
+            // bnt5
+            // 
+            bnt5.BackColor = Color.GhostWhite;
+            bnt5.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            bnt5.Location = new Point(269, 292);
+            bnt5.Name = "bnt5";
+            bnt5.Size = new Size(105, 92);
+            bnt5.TabIndex = 4;
+            bnt5.Text = "5";
+            bnt5.UseVisualStyleBackColor = false;
+            // 
+            // bnt6
+            // 
+            bnt6.BackColor = Color.GhostWhite;
+            bnt6.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            bnt6.Location = new Point(380, 292);
+            bnt6.Name = "bnt6";
+            bnt6.Size = new Size(105, 92);
+            bnt6.TabIndex = 5;
+            bnt6.Text = "6";
+            bnt6.UseVisualStyleBackColor = false;
+            // 
+            // bnt1
+            // 
+            bnt1.BackColor = Color.GhostWhite;
+            bnt1.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            bnt1.Location = new Point(158, 390);
+            bnt1.Name = "bnt1";
+            bnt1.Size = new Size(105, 92);
+            bnt1.TabIndex = 6;
+            bnt1.Text = "1";
+            bnt1.UseVisualStyleBackColor = false;
+            // 
+            // bnt2
+            // 
+            bnt2.BackColor = Color.GhostWhite;
+            bnt2.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            bnt2.Location = new Point(269, 390);
+            bnt2.Name = "bnt2";
+            bnt2.Size = new Size(105, 92);
+            bnt2.TabIndex = 7;
+            bnt2.Text = "2";
+            bnt2.UseVisualStyleBackColor = false;
+            // 
+            // bntVírgula
+            // 
+            bntVírgula.BackColor = Color.GhostWhite;
+            bntVírgula.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            bntVírgula.Location = new Point(158, 488);
+            bntVírgula.Name = "bntVírgula";
+            bntVírgula.Size = new Size(105, 92);
+            bntVírgula.TabIndex = 9;
+            bntVírgula.Text = ",";
+            bntVírgula.UseVisualStyleBackColor = false;
+            // 
+            // bnt0
+            // 
+            bnt0.BackColor = Color.GhostWhite;
+            bnt0.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            bnt0.Location = new Point(269, 488);
+            bnt0.Name = "bnt0";
+            bnt0.Size = new Size(105, 92);
+            bnt0.TabIndex = 10;
+            bnt0.Text = "0";
+            bnt0.UseVisualStyleBackColor = false;
+            // 
+            // bntDeletar
+            // 
+            bntDeletar.BackColor = Color.GhostWhite;
+            bntDeletar.Font = new Font("Candara", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            bntDeletar.Location = new Point(380, 488);
+            bntDeletar.Name = "bntDeletar";
+            bntDeletar.Size = new Size(105, 92);
+            bntDeletar.TabIndex = 11;
+            bntDeletar.Text = "Delete:";
+            bntDeletar.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Century Gothic", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(121, 22);
+            label1.TabIndex = 12;
+            label1.Text = "PERÍMETROS";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Candara", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(151, 82);
+            label2.Name = "label2";
+            label2.Size = new Size(48, 23);
+            label2.TabIndex = 15;
+            label2.Text = "Base";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Candara", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(151, 122);
+            label3.Name = "label3";
+            label3.Size = new Size(59, 23);
+            label3.TabIndex = 16;
+            label3.Text = "Altura";
+            // 
+            // fileSystemWatcher1
+            // 
+            fileSystemWatcher1.EnableRaisingEvents = true;
+            fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Candara", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(151, 164);
+            label4.Name = "label4";
+            label4.Size = new Size(71, 23);
+            label4.TabIndex = 17;
+            label4.Text = "Largura";
+            // 
+            // txtBase
+            // 
+            txtBase.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            txtBase.Location = new Point(220, 72);
+            txtBase.Name = "txtBase";
+            txtBase.Size = new Size(265, 29);
+            txtBase.TabIndex = 18;
+            // 
+            // txtLargura
+            // 
+            txtLargura.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            txtLargura.Location = new Point(220, 159);
+            txtLargura.Name = "txtLargura";
+            txtLargura.Size = new Size(265, 29);
+            txtLargura.TabIndex = 19;
+            txtLargura.Text = "c   ";
+            // 
+            // txtAltura
+            // 
+            txtAltura.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            txtAltura.Location = new Point(220, 112);
+            txtAltura.Name = "txtAltura";
+            txtAltura.Size = new Size(265, 29);
+            txtAltura.TabIndex = 20;
+            // 
+            // bntResultado
+            // 
+            bntResultado.BackColor = Color.GhostWhite;
+            bntResultado.Font = new Font("Century Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            bntResultado.Location = new Point(158, 586);
+            bntResultado.Name = "bntResultado";
+            bntResultado.Size = new Size(327, 52);
+            bntResultado.TabIndex = 21;
+            bntResultado.Text = "Calcule";
+            bntResultado.UseVisualStyleBackColor = false;
+            // 
+            // bnt3
+            // 
+            bnt3.BackColor = Color.GhostWhite;
+            bnt3.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            bnt3.Location = new Point(380, 391);
+            bnt3.Name = "bnt3";
+            bnt3.Size = new Size(105, 92);
+            bnt3.TabIndex = 22;
+            bnt3.Text = "3";
+            bnt3.UseVisualStyleBackColor = false;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.BackColor = Color.SeaShell;
+            flowLayoutPanel1.Location = new Point(91, 34);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(464, 631);
+            flowLayoutPanel1.TabIndex = 23;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Thistle;
+            ClientSize = new Size(800, 725);
+            Controls.Add(bnt3);
+            Controls.Add(bntResultado);
+            Controls.Add(txtAltura);
+            Controls.Add(txtLargura);
+            Controls.Add(txtBase);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(bntDeletar);
+            Controls.Add(bnt0);
+            Controls.Add(bntVírgula);
+            Controls.Add(bnt2);
+            Controls.Add(bnt1);
+            Controls.Add(bnt6);
+            Controls.Add(bnt5);
+            Controls.Add(bnt4);
+            Controls.Add(bnt9);
+            Controls.Add(bnt8);
+            Controls.Add(bnt7);
+            Controls.Add(flowLayoutPanel1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)bindingSource1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)fileSystemWatcher1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private BindingSource bindingSource1;
+        private Button bnt8;
+        private Button bnt9;
+        private Button bnt4;
+        private Button bnt5;
+        private Button bnt6;
+        private Button bnt1;
+        private Button bnt2;
+        private Button bntVírgula;
+        private Button bnt0;
+        private Button bntDeletar;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private FileSystemWatcher fileSystemWatcher1;
+        private Label label4;
+        private Button bntResultado;
+        private TextBox txtAltura;
+        private TextBox txtLargura;
+        private TextBox txtBase;
+        private Button bnt3;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Button bnt7;
+    }
+}
---
+++ Calculadora3.0Perímetros/Calculadora3.0Perímetros/Form1.cs
@@ -0,0 +1,20 @@
+namespace Calculadora3._0Perímetros
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
